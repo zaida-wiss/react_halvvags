96d9442732de8dc3a552edbcf00c75a7d1a5df13
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-03-13 20:52:20 +0100

    fortsätter med reducern

--- TeamPulse/src/reducers/checkInReducer.ts
+++ TeamPulse/src/reducers/checkInReducer.ts
@@ -1,18 +1,20 @@
 import {CheckIn} from "../types";
 
 export type CheckInAction =
-|{type: "ADD_CHECKIN";payload:}
+|{type: "ADD_CHECKIN";payload:Omit<CheckIn, "id" | "timestamp">}
 |{type: "REMOVE_CHECKIN";payload:}
 |{type: "CLEAR_DAY";payload:}
 
-export type checkInState = CheckIn[];
+export type CheckInState = CheckIn[];
 
 export function checkInReducer(
   state: CheckInState,
   action: CheckInAction): CheckInState {
     switch (action.type) {
-      case "ADD_CHECKIN";
+      case "ADD_CHECKIN":
       //Vad ska hända här? Tänk: du vill lägga till en ny check-in i listan.
       //Hur skapar du id och timestamp?
+      case "REMOVE_CHECKIN":
+      //Vad ska hända här? Tänk: du vill filtrera bort rätt checkin.
     }
   }
